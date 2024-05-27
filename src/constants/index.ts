3246e2875b9a593f4e086016169e025e0e7e3a2c
yunah <favoryun12@gmail.com>
2024-05-27 21:29:50 +0900

    edit. 컨텐츠 내용 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,46 +11,68 @@ export const MVP2_URLS = [
   {
     name: 'mvp1',
     url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/chekit_cover.png?alt=media&token=a7abb798-a9da-43d1-9f13-d350eabd4614',
-  },
-  {
-    name: 'mvp2',
-    url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/chekit_main.png?alt=media&token=a4c7410f-dc66-4b48-80a9-54815d78d288',
-  },
-  {
-    name: 'mvp3',
-    url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/std_main.png?alt=media&token=8976427e-fef5-445a-90a0-0e4c5f74be97',
+    desc: 'Chekit Intro Page',
+    subDesc: '체킷의 인트로 페이지 입니다.',
   },
   {
     name: 'mvp5',
     url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/chekit_signup.png?alt=media&token=c872f8f4-eab4-4165-bbbb-b9c10218b5f1',
+    desc: 'Chekit Signup Page',
+    subDesc: '회원가입 페이지입니다.',
+  },
+  {
+    name: 'mvp2',
+    url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/chekit_main.png?alt=media&token=a4c7410f-dc66-4b48-80a9-54815d78d288',
+    desc: 'Chekit Main Page',
+    subDesc: '로그인/회원가입 후 보여지는 메인 페이지입니다.',
+  },
+  {
+    name: 'mvp3',
+    url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/std_main.png?alt=media&token=8976427e-fef5-445a-90a0-0e4c5f74be97',
+    desc: 'Chekit Main Page',
+    subDesc: '로그인/회원가입 후 보여지는 메인 페이지입니다. 검사 종류별로 색의 구분을 주었습니다.',
   },
   {
     name: 'mvp12',
     url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/survey_cover.png?alt=media&token=4b638d24-9d8f-483c-a716-6c45e1bfc9cc',
+    desc: 'Chekit Survey Cover Page',
+    subDesc: '문진 페이지입니다.',
   },
   {
     name: 'mvp6',
     url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/chekit_survey1.png?alt=media&token=8b0df760-1b98-4653-ba68-d5a97cfc8192',
+    desc: 'Chekit Survey Page',
+    subDesc: '문진의 일부 페이지입니다.',
   },
   {
     name: 'mvp10',
     url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/result_strain.png?alt=media&token=103e0b24-6278-4d6f-acde-4fbb27116f14',
+    desc: 'Chekit Result Page - strain',
+    subDesc: '결과 페이지입니다.',
   },
   {
     name: 'mvp11',
     url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/result_strain2.png?alt=media&token=e02c96a4-282f-4d9d-8d30-45d8c88d0a6d',
+    desc: 'Chekit Result Page - strain',
+    subDesc: '결과 페이지입니다.',
   },
   {
     name: 'mvp8',
     url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/result_%20synthesis.png?alt=media&token=ca86477f-a12e-4511-98b8-d2b816ef8a65',
+    desc: 'Chekit Result Page - synthesis',
+    subDesc: '종합 결과 페이지입니다.',
   },
   {
     name: 'mvp9',
     url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/result_map.png?alt=media&token=b3b26cd1-f4a0-4eef-8108-58304d7729b1',
+    desc: 'Chekit Result Page - Hospital Map',
+    subDesc: '사용자 위치를 중심으로, 주변의 병원을 보여주는 페이지입니다.',
   },
   {
     name: 'mvp4',
     url: 'https://firebasestorage.googleapis.com/v0/b/yunaa-3cb91.appspot.com/o/lacto_list.png?alt=media&token=ec810256-54a9-4807-a6f0-ddfbe3539565',
+    desc: 'Chekit Lacto List Page',
+    subDesc: '검사 결과를 기반으로 해서 추천 유산균을 보여줍니다.',
   },
   {
     name: 'mvp7',
