8e9eb14c9686c979eaf0d661e324fa0dccca5715
yunah <favoryun12@gmail.com>
2024-07-19 13:45:03 +0900

    fix/verce.json 수정

--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -1,16 +1,15 @@
 import React, { FC } from 'react';
 import loadable from '@loadable/component';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import ChekitMvp2 from '@/components/ChekitMvp2';
 
 const Workspace = loadable(() => import('@layouts/Workspace'));
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/workspace" element={<Workspace />} />
-      <Route path="/*" element={<Workspace/>} />
+      {/* <Route path="/workspace" element={<Workspace />} /> */}
       <Route path="/workspace/*" element={<Workspace />} />
+      <Route path="/*" element={<Workspace/>} />
     </Routes>
   );
 };
